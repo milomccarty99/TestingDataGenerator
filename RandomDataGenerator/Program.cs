e1051d9bbdb92f5811f64c672c853fd6a363952f
Milo McCarty <milomccarty99@gmail.com>
2022-06-26 20:32:40 -0700

    commit

---
+++ RandomDataGenerator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+namespace RandomDataGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestingData td = new TestingData(new Random());
+            SchedulingInfo testSchedule = td.GenerateRandomSchedule(50, 30);
+            testSchedule.inc[1] = true;
+            VisualizeSchedule(testSchedule);
+            SwapFirstTwoInTestSchedule(testSchedule);
+            VisualizeSchedule(testSchedule);
+        }
+
+
+        static void VisualizeSchedule(SchedulingInfo sched)
+        {
+            for(int i = 0; i < sched.length; i ++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+            for(int i = 0; i < sched.length; i++)
+            {
+                if (sched.inc[i])
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                }
+                for (int p = 0; p < sched.s[i]; p++)
+                {
+                    Console.Write(" "); // blank space character
+                }
+                Console.Write("[");
+                for(int p = sched.s[i]; p < sched.f[i]; p++)
+                {
+                    Console.Write("=");
+                }
+                Console.Write("]");
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        static void SwapFirstTwoInTestSchedule(SchedulingInfo testSchedule)
+        {
+            testSchedule.Swap(0, 1);
+        }
+    }
+}
---
+++ RandomDataGenerator/TestingData.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomDataGenerator
+{
+    class TestingData
+    {
+        int highRange;
+        int lowRange;
+        int length;
+        Random rand;
+        public TestingData(Random random)
+        {
+            this.rand = random; 
+            
+        }
+
+        //outputs two correlated arrays S and F as SchedulingInfo for scheduling problem
+        public SchedulingInfo GenerateRandomSchedule(int n, int cNum)
+        {
+            SchedulingInfo sched = new SchedulingInfo(cNum);
+            
+            for(int i = 0; i < cNum; i++)
+            {
+                int classStart = rand.Next(n- 1);
+                int classFinish = rand.Next(classStart+ 1, n);
+                sched.s[i] = classStart;
+                sched.f[i] = classFinish;
+            }
+            return sched;
+        }
+        // matrix of values for sample data to text file
+        //integer array of values between high and low.
+        //double array of values between high and low.
+        //float array of values between high and low.
+
+        SchedulingInfo ScheduleFinishMergeSort(SchedulingInfo sched)
+        {
+            return sched;
+        }
+}
+
+
+    class SchedulingInfo
+    {
+        public int[] s;
+        public int[] f;
+        public bool[] inc;
+        public int length;
+        public SchedulingInfo(int len)
+        {
+            this.length = len;
+            s = new int[len];
+            f = new int[len];
+            inc = new bool[len];
+        }
+        public SchedulingInfo GetSubSection(int start, int end)
+        {
+            SchedulingInfo result = new SchedulingInfo(end - start);
+            for(int i = start; i < end; i++)
+            {
+                result.s[i - start] = this.s[i];
+                result.f[i - start] = this.f[i];
+                result.inc[i - start] = this.inc[i];
+            }
+            return result;
+        }
+
+        public void Swap(int first, int second)
+        {
+            int ste = this.s[first];
+            int fte = this.f[first];
+            bool incte = this.inc[first];
+            this.s[first] = this.s[second];
+            this.f[first] = this.f[second];
+            this.inc[first] = this.inc[second];
+            this.s[second] = ste;
+            this.f[second] = fte;
+            this.inc[second] = incte;
+        }
+    }
+}
